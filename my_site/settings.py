727f643b164a7984ffb57fb02ef7abc5c82a8cd9
Nikul <p.nikul6403@gmail.com>
2025-06-21 22:35:37 +0530

    add dj_database_url

--- my_site/settings.py
+++ my_site/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from django.conf.global_settings import MEDIA_ROOT, STATICFILES_DIRS, MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
