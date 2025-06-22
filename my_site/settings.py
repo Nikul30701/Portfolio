c3e5fbe5b9bd902fb14b111d7f06389e86f6f020
Nikul <p.nikul6403@gmail.com>
2025-06-22 19:36:44 +0530

    Prepare for Render deployments

--- my_site/settings.py
+++ my_site/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True if 'postgresql' in os.environ.get('DATABASE_URL', '') else False
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
